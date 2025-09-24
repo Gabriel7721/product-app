882a548a22469dba3415461ca5b185a5d7b457cf
Bùi Trần Anh Trí <116139790+Gabriel7721@users.noreply.github.com>
2025-09-24 10:57:43 +0700

    Building a Game Card

---
+++ src/components/GameCards.tsx
@@ -0,0 +1,36 @@
+import type { Game } from "@/hooks/useGames";
+import { Button, Card, Image, Text } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCards = ({ game }: Props) => {
+  return (
+    <div>
+      <Card.Root maxW="sm" overflow="hidden">
+        <Image src={game.background_image} alt="Game Background Image" />
+        <Card.Body gap="2">
+          <Card.Title>{game.name}</Card.Title>
+          <Card.Description>
+            This sofa is perfect for modern tropical spaces, baroque inspired
+            spaces.
+          </Card.Description>
+          <Text
+            textStyle="2xl"
+            fontWeight="medium"
+            letterSpacing="tight"
+            mt="2">
+            $450
+          </Text>
+        </Card.Body>
+        <Card.Footer gap="2">
+          <Button variant="solid">Buy now</Button>
+          <Button variant="ghost">Add to cart</Button>
+        </Card.Footer>
+      </Card.Root>
+    </div>
+  );
+};
+
+export default GameCards;
